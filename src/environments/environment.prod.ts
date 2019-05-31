ce05f30402b45b4775c61b01076cef928ab588c7
tungnxu <tungnxu@outlook.com>
2019-05-31 23:31:11 +0700

    update ui

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA4-901xnz1FCTwu95mQe4UhXtTQ2jliaw",
+    authDomain: "ng7-authen-app.firebaseapp.com",
+    databaseURL: "https://ng7-authen-app.firebaseio.com",
+    projectId: "ng7-authen-app",
+    storageBucket: "ng7-authen-app.appspot.com",
+    messagingSenderId: "734950950240",
+    appId: "1:734950950240:web:39edb53bf288a0e7"
+  }
 };
